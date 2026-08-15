dbd35912361212f8f42f9f2c26338de927e875aa
web3light07 <lightokey839@gmail.com>
2026-08-15 17:36:45 +0100

    Fix backend JWT typing

--- backend/src/plugins/jwt.ts
+++ backend/src/plugins/jwt.ts
@@ -93,7 +93,8 @@ export function sessionCookieOptions() {
  */
 export async function tryGetAdminId(request: FastifyRequest): Promise<string | null> {
   try {
-    const { adminId } = await request.jwtVerify({ onlyCookie: true });
+    await request.jwtVerify({ onlyCookie: true });
+    const { adminId } = request.user;
     return adminId;
   } catch {
     return null;
